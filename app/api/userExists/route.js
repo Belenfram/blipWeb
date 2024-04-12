b28390d98b928549dc1eb0c6224ffe2a35c004e9
Belenfram <huchimonofrey.b@gmail.com>
2024-04-12 12:44:06 -0600

    Actualizacion 7

--- app/api/userExists/route.js
+++ app/api/userExists/route.js
@@ -1,4 +1,4 @@
-/*import { connectMongoDB } from "@/lib/mongodb";
+import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
 import { NextResponse } from "next/server";
 
@@ -12,23 +12,5 @@ export async function POST(req){
     } catch (error) {
         console.log(error);
     }
-}*/
-
-import { connectMongoDB } from "@/lib/mongodb";
-import User from "@/models/user";
-import { NextResponse } from "next/server";
-
-export async function POST(req) {
-    try {
-        await connectMongoDB();
-        const { email } = await req.json();
-        const user = await User.findOne({ email }).select("_id");
-        console.log("user: ", user);
-        //return NextResponse.json({ user }, { status: 200 });
-        return NextResponse.json({ user }, 200);
-    } catch (error) {
-        console.log(error);
-        // Devolver una respuesta de error en caso de fallo
-        return NextResponse.error(error);
-    }
 }
+
