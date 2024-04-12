cd218f97312b8227d03e2e9b0835311c17689c2d
Belenfram <huchimonofrey.b@gmail.com>
2024-04-12 14:43:02 -0600

    Actualizacion 9

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -59,7 +59,31 @@ export const authOptions = {
             name: "credentials",
             credentials: {},
 
-            async authorize(credentials){
+            async authorize(credentials) {
+                const { email, password } = credentials;
+              
+                try {
+                  await connectMongoDB();
+                  const user = await User.findOne({ email });
+              
+                  if (!user) {
+                    return { error: "Usuario no encontrado" }; // Error de usuario no encontrado
+                  }
+              
+                  const passwordsMatch = await bcrypt.compare(password, user.password);
+              
+                  if (!passwordsMatch) {
+                    return { error: "Contraseña incorrecta" }; // Error de contraseña incorrecta
+                  }
+              
+                  return user;
+                } catch (error) {
+                  console.error("Error al autorizar usuario:", error);
+                  return { error: "Ocurrió un error durante la autenticación" }; // Error genérico para problemas inesperados
+                }
+              },
+
+            /*async authorize(credentials){
                 const {email, password} = credentials;
                 
                 try {
@@ -80,13 +104,14 @@ export const authOptions = {
                 } catch (error) {
                     console.log("Error: ", error);
                 }
-            },
+            },*/
         }),
     ],
     session: {
         strategy: "jwt",
     },
-    secret: "proyectoBlipcito2000",
+    //secret: "proyectoBlipcito2000",
+    secret: process.env.NEXTAUTH_SECRET,
     pages: {
         signIn: "/",
     },
