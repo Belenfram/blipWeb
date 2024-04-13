83576f9ee4836cb2b20ea2dca53e6b6a592e15da
Belenfram <huchimonofrey.b@gmail.com>
2024-04-12 20:06:03 -0600

    Actualizacion 15

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -84,7 +84,7 @@ import LogoutButton from './Signout';
 export default function Navbar() {
 
   return (
-    <nav className="bg-teal-600 fixed top-0 w-full z-10">
+    /*<nav className="bg-teal-600 fixed top-0 w-full z-10">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
@@ -110,13 +110,52 @@ export default function Navbar() {
               </div>
             </div>
           </div>
-          {/* Botón de cierre de sesión */}
           <div className="flex items-center">
             <LogoutButton />
           </div>
         </div>
       </div>
-    </nav>
+    </nav>*/
+    <nav className="bg-teal-600 fixed top-0 w-full z-10">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div className="relative flex items-center justify-between h-16">
+        <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="flex-shrink-0 flex items-center">
+          </div>
+          <div className="hidden sm:block sm:ml-6">
+            <div className="flex space-x-4">
+              <div className="text-gray-100 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <Link href="/dashboard">
+                  Paquetes
+                </Link>
+              </div>
+              <div className="text-gray-100 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <Link href="/mqtt">
+                  MQTT
+                </Link>
+              </div>
+              <div className="text-gray-100 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <Link href="/about">
+                  Acerca de
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Botón de menú para pantallas pequeñas */}
+        <div className="sm:hidden">
+          <button className="text-gray-100 hover:bg-gray-700 hover:text-white px-4 py-2 rounded-md text-sm font-medium">
+            Menú
+          </button>
+        </div>
+        {/* Botón de cierre de sesión */}
+        <div className="flex items-center">
+          <LogoutButton />
+        </div>
+      </div>
+    </div>
+  </nav>
+
   );
 }
 
